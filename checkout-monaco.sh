f691af5b65462446b6e3e1fd3ff41c0564a5b632
Brian Jenkins <brianjenkins94@gmail.com>
2023-08-23 13:03:19 -0400

    Tidy

---
+++ checkout-monaco.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+rm -rf monaco
+
+if [[ "$(uname -s)" == MINGW* ]]; then
+    git clone https://github.com/CodinGame/monaco-vscode-api.git monaco
+    cd monaco
+else
+    git clone --no-checkout --depth 1 --filter=tree:0 --sparse https://github.com/CodinGame/monaco-vscode-api.git monaco
+    cd monaco
+    git sparse-checkout set --no-cone "!/*" "/demo"
+    git checkout
+fi
+
+cd demo
+npm install --ignore-scripts
+npm install vscode@npm:@codingame/monaco-vscode-api -f
+node node_modules/vscode/monaco-treemending.js
+#npm start
+
+cd "$CWD"
