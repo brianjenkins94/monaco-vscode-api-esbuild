4de25815783e3a9ea398bb60f07d95cc4e475d79
Brian Jenkins <brianjenkins94@gmail.com>
2023-11-08 17:58:20 -0500

    Some kind of problem with `await getApi();`

--- checkout-monaco.sh
+++ checkout-monaco.sh
@@ -5,17 +5,18 @@ CWD=$(pwd)
 rm -rf monaco-vscode-api
 
 if [[ "$(uname -s)" == MINGW* ]]; then
-    git clone https://github.com/CodinGame/monaco-vscode-api.git
-    cd monaco-vscode-api
+	git clone https://github.com/CodinGame/monaco-vscode-api.git
+	cd monaco-vscode-api
 else
-    git clone --no-checkout --depth 1 --filter=tree:0 --sparse https://github.com/CodinGame/monaco-vscode-api.git
-    cd monaco-vscode-api
-    git sparse-checkout set --no-cone "!/*" "/demo"
-    git checkout
+	git clone --no-checkout --depth 1 --filter=tree:0 --sparse https://github.com/CodinGame/monaco-vscode-api.git
+	cd monaco-vscode-api
+	git sparse-checkout set --no-cone "!/*" "/demo"
+	git checkout
 fi
 
 cd demo
 npm uninstall dockerode
+# FIXME: This doesn't work on macOS
 sed -i "s/file:[^\"]*/latest/g" package.json
 npm install --ignore-scripts vscode@npm:@codingame/monaco-vscode-api
 node node_modules/vscode/monaco-treemending.js
